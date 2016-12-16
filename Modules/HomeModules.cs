0cf6df73c1d311fdbdca8728577bc80875b5c86e
Loren Glenn <lorencglenn@gmail.com>
2016-12-16 11:13:54 -0800

    add details page for venues and bands

--- Modules/HomeModules.cs
+++ Modules/HomeModules.cs
@@ -10,6 +10,8 @@ namespace Tracker
     public HomeModule()
     {
       Get["/"] = _ => View["index.cshtml"];
+      Get["/add-band"] =_=> View["add-band.cshtml"];
+      Get["/add-venue"] =_=> View["add-venue.cshtml"];
 
       Get["/bands"] =_=> {
         List<Band> allBands = Band.GetAll();
@@ -18,6 +20,47 @@ namespace Tracker
 
       Get["/venues"] =_=> {
         List<Venue> allVenues = Venue.GetAll();
+        return View["venues.cshtml", allVenues];
+      };
+
+      Get["/band/{id}"] = parameters =>
+      {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Band selectedBand = Band.Find(parameters.id);
+        List<Venue> venues = selectedBand.GetVenues();
+        List<Venue> allVenues = Venue.GetAll();
+        model.Add("band", selectedBand);
+        model.Add("venues", venues);
+        model.Add("allVenues", allVenues);
+        return View["band.cshtml", model];
+      };
+      Get["/venue/{id}"] = parameters =>
+      {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Venue selectedVenue = Venue.Find(parameters.id);
+        List<Band> Bands = selectedVenue.GetBands();
+        List<Band> allBands = Band.GetAll();
+        model.Add("venue", selectedVenue);
+        model.Add("bands", Bands);
+        model.Add("allBands", allBands);
+        return View["venue.cshtml", model];
+      };
+      Post["/band/associate-venue"] = _ =>
+      {
+        Band band = Band.Find(Request.Form["band-id"]);
+        Venue venue = Venue.Find(Request.Form["venue-id"]);
+        band.AddVenue(venue);
+        List<Band> allBands = new List<Band>{};
+        allBands = Band.GetAll();
+        return View["bands.cshtml", allBands];
+      };
+      Post["/venue/associate-band"] = _ =>
+      {
+        Venue venue = Venue.Find(Request.Form["venue-id"]);
+        Band band = Band.Find(Request.Form["band-id"]);
+        venue.AddBand(band);
+        List<Venue> allVenues = new List<Venue>{};
+        allVenues = Venue.GetAll();
         return View["venues.cshtml", allVenues];
       };
 
